968300ec835c79a8c1cb63324c91b282a9dc8a58
Ralph Bankston <ralph@Ralphs-MacBook-Air.local>
2022-08-04 21:53:00 -0400

    showing

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ function addItem(array, itemName, itemCategory, itemPrice, dateBought) {
     return array.push({"Name": itemName, "Category": itemCategory,  "Price": itemPrice, "purchaseDate": dateBought});
 }
 
+ 
